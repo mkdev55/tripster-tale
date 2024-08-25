b2e4518afca4eee4af1c30246bf2fbfe7673d57b
mengkheang <mengkheangbill24@gmail.com>
2024-08-25 11:02:14 +0700

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,24 +34,25 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-8 shadow-lg rounded-b">
-                <div class="card-body bg-[#f1f5f9] border-t mt-8">
+            <div class="col-md-8 shadow-lg rounded-b ">
+                <div class="card-body  border-t my-8 border-0">
 
-                    <div>
-                        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only ">Search</label>
-                        <div class="relative">
-                            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                                <svg class="w-4 h-4 text-gray-500 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                    fill="none" viewBox="0 0 20 20">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                        stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                    <div class="flex justify-evenly items-center">
+                        <div class="w-[80%]">
+                            <div class="relative">
+                              <input type="search" id="default-search" class="block w-full py-3 px-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 shadow" placeholder="Search Here..." required />
+                              <div class="absolute inset-y-0 end-0 flex items-center pr-3 pointer-events-none">
+                                <svg class="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                                 </svg>
+                              </div>
                             </div>
-                            <input type="search" id="default-search"
-                                class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
-                                placeholder="Search Mockups, Logos..." required />
+                          </div>
+                        <div>
+                            <img src="settings.png" alt="" class="w-10 h-10 cursor-pointer hover:ring-4 hover:ring-gray-400 hover:ring-offset-2 hover:ring-offset-gray-100">
                         </div>
                     </div>
+
 
 
                 </div>
